d585d893904bd694773342cb7f15ae0b8c4675b4
Elgars Upitis <elgars.upitis@gmail.com>
2021-12-17 11:13:37 +0200

    Implement logInWithUser()

--- cypress/integration/lv.sportsdirect.com/userFlow.spec.js
+++ cypress/integration/lv.sportsdirect.com/userFlow.spec.js
@@ -4,19 +4,19 @@ import { LoginPage } from "../../pageObjects/pages/LoginPage";
 import { HomePage } from "../../pageObjects/pages/HomePage";
 
 describe("Testing user creation and login flow.", () => {
-  it("Creating a new user.", () => {
+  it.skip("Creating a new user.", () => {
     HomePage.goToLoginPage();
     LoginPage.goToNewUserForm();
     CreateUserPage.enterUserData();
     //There isn't a unique message for new user registration
     //If successful, newly created user is logged in
     //Verifying test case is the same as a logged in user verify 
-    //HomePage.verifyLogin();
+    //TODO: HomePage.verifyLoginStatus();
   });
 
-  it.skip("Logging in with a registered user", () => {
+  it("Logging in with a registered user", () => {
     HomePage.goToLoginPage();
-    LoginPage.logInWithUser();
-    HomePage.verifyLogin();
+    LoginPage.logInWithUser("registered_user");
+    //TODO: HomePage.verifyLoginStatus();
   });
 });
--- cypress/pageObjects/pages/LoginPage.js
+++ cypress/pageObjects/pages/LoginPage.js
@@ -1,10 +1,21 @@
 /// <reference types="Cypress" />
 import { BasePage } from "../BasePage";
 const CONFIRM_NEW_USER_FORM = ".Login > .row > .col-xs-12 > h1";
+const LOGIN_EMAIL_FIELD = "#Login_EmailAddress";
+const LOGIN_PASSWORD_FIELD = "#Login_Password";
+const LOGIN_BUTTON = "#LoginButton";
 
 export class LoginPage extends BasePage {
   static goToNewUserForm() {
     cy.visit("/registration?returnUrl=%2f");
     cy.get(CONFIRM_NEW_USER_FORM).should("have.text", "Registration");
+  }
+
+  static logInWithUser(user = "registered_user") {
+      cy.fixture("userData").then((fixture) => {
+          this.type(LOGIN_EMAIL_FIELD, fixture[user].email);
+          this.type(LOGIN_PASSWORD_FIELD, fixture[user].password);
+      });
+      this.click(LOGIN_BUTTON);
   }
 }
